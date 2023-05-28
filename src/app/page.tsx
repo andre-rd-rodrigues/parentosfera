93d44854a456664a3e3d421ff3194ef76484f744
Andre Rodrigues <andre.rodrigues@paytrix.io>
2023-05-28 01:31:33 +0100

    improve styles and fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,41 @@
 "use client";
 import styles from "./page.module.scss";
 import Typewriter from "typewriter-effect";
+import { lato, montserrat, poppins } from "@/fonts/fonts";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <video autoPlay loop muted>
-        <source src="/family.mp4" type="video/mp4" />
-      </video>
-      <div className={styles.contentContainer}>
-        <Typewriter
-          options={{
-            strings: ["#PARENTOSFERA"],
-            autoStart: true,
-            loop: true,
-            wrapperClassName: styles.typewriter
-          }}
-        />
-        <p>
-          Se chegou até aqui é porque a parentalidade é importante para si.
-          Inscreva-se agora e receba contéudo em primeira mão, teremos surpresas
-          brevemente.
-        </p>
-        <button>INSCRIÇÃO</button>
-      </div>
-    </main>
+    <>
+      <style jsx global>{`
+        :root {
+          --montserrat: ${montserrat.style.fontFamily};
+          --lato: ${lato.style.fontFamily};
+          --poppins: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <main className={styles.main}>
+        <video autoPlay loop muted preload="/images/initial.png">
+          <source src="/family.mp4" type="video/mp4" />
+        </video>
+        <div className={styles.contentContainer}>
+          <div className={styles.title}>
+            <Typewriter
+              options={{
+                strings: ["#PARENTOSFERA"],
+                autoStart: true,
+                loop: true,
+                wrapperClassName: styles.typewriter
+              }}
+            />
+          </div>
+          <p>
+            Se chegou até aqui é porque a parentalidade é importante para si.
+            Inscreva-se agora e receba contéudo em primeira mão, teremos
+            surpresas brevemente.
+          </p>
+          <button>INSCRIÇÃO</button>
+        </div>
+      </main>
+    </>
   );
 }
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,19 @@
+import { Montserrat, Lato, Poppins } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["600"]
+});
+
+export const lato = Lato({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300"]
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["500"]
+});
